02e2369897b909d4170f0e26b445d150d6dcedd4
v_guoyonggang01 <shengeyisi@163.com>
2018-11-22 16:51:13 +0800

    spring cloud 2

---
+++ demo3/src/main/java/com/springcloud/demo3/pojo/Person.java
@@ -0,0 +1,56 @@
+package com.springcloud.demo3.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    private Integer id;
+    private String name;
+    private String gender;
+    private Integer age;
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
